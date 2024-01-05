67fdf46235cab6cec5161a328eed860f91a50784
lankateju <126489998+lankateju@users.noreply.github.com>
2024-01-05 22:28:13 +0530

    Create vpc.tf

---
+++ vpc.tf
@@ -0,0 +1,457 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
+// Creating VPC
+resource "aws_vpc" "demo-vpc" {
+  cidr_block = "10.0.0.0/16"
+}
+
+// Creating Public Subnets
+resource "aws_subnet" "public_subnet_1" {
+  vpc_id     = aws_vpc.demo-vpc.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "ap-south-1a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "public-subnet-1"
+  }
+}
+
+resource "aws_subnet" "public_subnet_2" {
+  vpc_id     = aws_vpc.demo-vpc.id
+  cidr_block = "10.0.2.0/24"
+  availability_zone = "ap-south-1b"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "public-subnet-2"
+  }
+}
+
+resource "aws_subnet" "public_subnet_3" {
+  vpc_id     = aws_vpc.demo-vpc.id
+  cidr_block = "10.0.3.0/24"
+  availability_zone = "ap-south-1c"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "public-subnet-3"
+  }
+}
+
+// Creating Internet Gateway
+resource "aws_internet_gateway" "demo-igw" {
+  vpc_id = aws_vpc.demo-vpc.id
+
+  tags = {
+    Name = "demo-igw"
+  }
+}
+
+// Creating Route Table for Public Subnets
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.demo-vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.demo-igw.id
+  }
+
+  tags = {
+    Name = "public-rt"
+  }
+}
+
+// Associating Public Subnets with the Public Route Table
+
+resource "aws_route_table_association" "public_subnet_1_association" {
+  subnet_id      = aws_subnet.public_subnet_1.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+resource "aws_route_table_association" "public_subnet_2_association" {
+  subnet_id      = aws_subnet.public_subnet_2.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+resource "aws_route_table_association" "public_subnet_3_association" {
+  subnet_id      = aws_subnet.public_subnet_3.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+// Creating Private Subnets
+resource "aws_subnet" "private_subnet_1" {
+  vpc_id     = aws_vpc.demo-vpc.id
+  cidr_block = "10.0.4.0/24"
+  availability_zone = "ap-south-1a"
+
+  tags = {
+    Name = "private-subnet-1"
+  }
+}
+
+resource "aws_subnet" "private_subnet_2" {
+  vpc_id     = aws_vpc.demo-vpc.id
+  cidr_block = "10.0.5.0/24"
+  availability_zone = "ap-south-1b"
+
+  tags = {
+    Name = "private-subnet-2"
+  }
+}
+
+resource "aws_subnet" "private_subnet_3" {
+  vpc_id     = aws_vpc.demo-vpc.id
+  cidr_block = "10.0.6.0/24"
+  availability_zone = "ap-south-1c"
+
+  tags = {
+    Name = "private-subnet-3"
+  }
+}
+ 
+resource "aws_subnet" "private_subnet_4" {
+  vpc_id            = aws_vpc.demo-vpc.id
+  cidr_block        = "10.0.7.0/24"
+  availability_zone = "ap-south-1a"
+
+  tags = {
+    Name = "private-subnet-4"
+  }
+}
+
+resource "aws_subnet" "private_subnet_5" {
+  vpc_id            = aws_vpc.demo-vpc.id
+  cidr_block        = "10.0.8.0/24"
+  availability_zone = "ap-south-1b"
+
+  tags = {
+    Name = "private-subnet-5"
+  }
+}
+
+resource "aws_subnet" "private_subnet_6" {
+  vpc_id            = aws_vpc.demo-vpc.id
+  cidr_block        = "10.0.9.0/24"
+  availability_zone = "ap-south-1c"
+
+  tags = {
+    Name = "private-subnet-6"
+  }
+}
+
+
+// Creating Route Table for Private Subnets
+resource "aws_route_table" "private_rt" {
+  vpc_id = aws_vpc.demo-vpc.id
+
+  tags = {
+    Name = "private-rt"
+  }
+}
+
+// Associating Private Subnets with the Private Route Table
+resource "aws_route_table_association" "private_subnet_1_association" {
+  subnet_id      = aws_subnet.private_subnet_1.id
+  route_table_id = aws_route_table.private_rt.id
+}
+
+resource "aws_route_table_association" "private_subnet_2_association" {
+  subnet_id      = aws_subnet.private_subnet_2.id
+  route_table_id = aws_route_table.private_rt.id
+}
+
+resource "aws_route_table_association" "private_subnet_3_association" {
+  subnet_id      = aws_subnet.private_subnet_3.id
+  route_table_id = aws_route_table.private_rt.id
+}
+
+// Associating Private Subnets with the Private Route Table
+resource "aws_route_table_association" "private_subnet_4_association" {
+  subnet_id      = aws_subnet.private_subnet_4.id
+  route_table_id = aws_route_table.private_rt.id
+}
+
+resource "aws_route_table_association" "private_subnet_5_association" {
+  subnet_id      = aws_subnet.private_subnet_5.id
+  route_table_id = aws_route_table.private_rt.id
+}
+
+resource "aws_route_table_association" "private_subnet_6_association" {
+  subnet_id      = aws_subnet.private_subnet_6.id
+  route_table_id = aws_route_table.private_rt.id
+}
+
+
+// Creating Security Group
+resource "aws_security_group" "demo-vpc-sg" {
+  name        = "allow_tls"
+  vpc_id      = aws_vpc.demo-vpc.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_tls"
+  }
+}
+
+// Creating NAT Gateway
+#NAT Gateway associated with the public subnet. The NAT Gateway allows instances in private subnets to access the internet.
+resource "aws_nat_gateway" "nat_gateway" {
+  allocation_id = aws_instance.nat_instance.id
+  subnet_id     = aws_subnet.public_subnet_1.id
+}
+
+// Creating an Elastic IP for NAT Gateway
+resource "aws_instance" "nat_instance" {
+  ami                    = "ami-0a0f1259dd1c90938"
+  instance_type          = "t2.micro"
+  key_name               = "teju109152_linux"  # Replace with your actual key pair name
+  subnet_id              = aws_subnet.public_subnet_1.id
+  associate_public_ip_address = true
+
+  tags = {
+    Name = "nat-instance"
+  }
+}
+
+Creating an AWS EC2 Instance
+resource "aws_instance" "demo_server" {
+  ami                    = "ami-0a0f1259dd1c90938"
+  key_name               = "teju09152_linux"  # Replace with your actual key pair name
+  instance_type          = "t2.micro"
+  subnet_id              = aws_subnet.public_subnet_1.id
+  vpc_security_group_ids = [aws_security_group.demo-vpc-sg.id]
+}
+
+// Creating security groups for kubernetes
+
+resource "aws_security_group" "worker_node_sg" {
+  name        = "eks-test"
+  description = "Allow ssh inbound traffic"
+  vpc_id      =  aws_vpc.demo-vpc.id
+
+  ingress {
+    description      = "ssh access to public"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+}
+
+//creating roles,policies and attaching roles
+
+resource "aws_iam_role" "master" {
+  name = "ed-eks-master"
+
+  assume_role_policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "eks.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+POLICY
+}
+
+resource "aws_iam_role_policy_attachment" "AmazonEKSClusterPolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.master.name
+}
+
+resource "aws_iam_role_policy_attachment" "AmazonEKSServicePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSServicePolicy"
+  role       = aws_iam_role.master.name
+}
+
+resource "aws_iam_role_policy_attachment" "AmazonEKSVPCResourceController" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSVPCResourceController"
+  role       = aws_iam_role.master.name
+}
+
+resource "aws_iam_role" "worker" {
+  name = "ed-eks-worker"
+
+  assume_role_policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+POLICY
+}
+
+resource "aws_iam_policy" "autoscaler" {
+  name   = "ed-eks-autoscaler-policy"
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "autoscaling:DescribeAutoScalingGroups",
+        "autoscaling:DescribeAutoScalingInstances",
+        "autoscaling:DescribeTags",
+        "autoscaling:DescribeLaunchConfigurations",
+        "autoscaling:SetDesiredCapacity",
+        "autoscaling:TerminateInstanceInAutoScalingGroup",
+        "ec2:DescribeLaunchTemplateVersions"
+      ],
+      "Effect": "Allow",
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+
+}
+
+resource "aws_iam_role_policy_attachment" "AmazonEKSWorkerNodePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role       = aws_iam_role.worker.name
+}
+
+resource "aws_iam_role_policy_attachment" "AmazonEKS_CNI_Policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role       = aws_iam_role.worker.name
+}
+
+resource "aws_iam_role_policy_attachment" "AmazonSSMManagedInstanceCore" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+  role       = aws_iam_role.worker.name
+}
+
+resource "aws_iam_role_policy_attachment" "AmazonEC2ContainerRegistryReadOnly" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = aws_iam_role.worker.name
+}
+
+resource "aws_iam_role_policy_attachment" "x-ray" {
+  policy_arn = "arn:aws:iam::aws:policy/AWSXRayDaemonWriteAccess"
+  role       = aws_iam_role.worker.name
+}
+resource "aws_iam_role_policy_attachment" "s3" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess"
+  role       = aws_iam_role.worker.name
+}
+
+resource "aws_iam_role_policy_attachment" "autoscaler" {
+  policy_arn = aws_iam_policy.autoscaler.arn
+  role       = aws_iam_role.worker.name
+}
+
+resource "aws_iam_instance_profile" "worker" {
+  depends_on = [aws_iam_role.worker]
+  name       = "ed-eks-worker-new-profile"
+  role       = aws_iam_role.worker.name
+}
+
+###############################################################################################################
+resource "aws_eks_cluster" "eks" {
+  name = "ed-eks-01"
+  role_arn = aws_iam_role.master.arn
+
+  vpc_config {
+    subnet_ids = [aws_subnet.private_subnet_1.id,
+      aws_subnet.private_subnet_2.id,
+      aws_subnet.private_subnet_3.id,
+      aws_subnet.private_subnet_4.id,
+      aws_subnet.private_subnet_5.id,
+      aws_subnet.private_subnet_6.id,
+    ]
+  }
+  
+  depends_on = [
+    aws_iam_role_policy_attachment.AmazonEKSClusterPolicy,
+    aws_iam_role_policy_attachment.AmazonEKSServicePolicy,
+    aws_iam_role_policy_attachment.AmazonEKSVPCResourceController,
+    aws_iam_role_policy_attachment.AmazonEKSVPCResourceController,
+  ]
+
+}
+#################################################################################################################
+
+resource "aws_eks_node_group" "backend" {
+  cluster_name    = aws_eks_cluster.eks.name
+  node_group_name = "dev"
+  node_role_arn   = aws_iam_role.worker.arn
+  subnet_ids = [aws_subnet.private_subnet_1.id,
+      aws_subnet.private_subnet_2.id,
+      aws_subnet.private_subnet_3.id,
+      aws_subnet.private_subnet_4.id,
+      aws_subnet.private_subnet_5.id,
+      aws_subnet.private_subnet_6.id,
+    ]
+  capacity_type = "ON_DEMAND"
+  disk_size = "20"
+  instance_types = ["t2.small"]
+  remote_access {
+    ec2_ssh_key = "rtp-03"
+    source_security_group_ids = [aws_security_group.demo-vpc-sg.id]
+  } 
+  
+  labels =  tomap({env = "dev"})
+  
+  scaling_config {
+    desired_size = 2
+    max_size     = 3
+    min_size     = 1
+  }
+
+  update_config {
+    max_unavailable = 1
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.AmazonEKSWorkerNodePolicy,
+    aws_iam_role_policy_attachment.AmazonEKS_CNI_Policy,
+    aws_iam_role_policy_attachment.AmazonEC2ContainerRegistryReadOnly,
+    #aws_subnet.pub_sub1,
+    #aws_subnet.pub_sub2,
+  ]
+   security_group_ids = [aws_security_group.demo-vpc-sg.id]
+   vpc_id = aws_vpc.demo-vpc.id
+    subnet_ids = [aws_subnet.private_subnet_1.id,
+      aws_subnet.private_subnet_2.id,
+      aws_subnet.private_subnet_3.id,
+      aws_subnet.private_subnet_4.id,
+      aws_subnet.private_subnet_5.id,
+      aws_subnet.private_subnet_6.id,
+    ]
+}
